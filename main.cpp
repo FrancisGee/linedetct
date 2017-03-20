ce647377791b7cd26228ba98c33e8bce14b0e34c
FrancisGee <gaoyikang2014@Gmail.com>
2017-03-20 15:18:26 +0800

    封装Hough检测车道画图函数

--- main.cpp
+++ main.cpp
@@ -49,7 +49,9 @@ using namespace std;
 
 void showBasicInformation(VideoCapture &input);
 
-Mat cannyExtract(Mat &imgROI);
+Mat cannyExtract(Mat &imgROI, bool showCanny);
+
+void drawHoughDectedLines(vector<Vec2f> &lines, Mat &result, bool showHough);
 
 int main(int argc, char *argv[]) {
     int houghVote = 200;
@@ -59,8 +61,7 @@ int main(int argc, char *argv[]) {
     //Set up windows
     //for Debug
     bool showOriginal = false;
-    bool showCanny = false;
-    bool showHough = false;
+    bool showCanny = true;
     bool showHoughP = false;
 
     string window_name = "Processed Video";
@@ -106,17 +107,6 @@ int main(int argc, char *argv[]) {
         Rect roi(0, image.cols / 3, image.cols - 1, image.rows - image.cols / 3);// set the ROI for the image
 
         Mat imgROI = image(roi);
-        Scalar val = Scalar(0, 0, 0);//??????
-        copyMakeBorder(imgROI, imgROI, 2, 2, 2, 2, BORDER_CONSTANT, val);   //???
-
-
-
-        // Display Canny image
-
-
-
-
-
 
 
         //霍夫线段检测
@@ -132,7 +122,7 @@ int main(int argc, char *argv[]) {
 
 
         //用canny算子对ROI进行特征提取
-        Mat contours = cannyExtract(imgROI);
+        Mat contours = cannyExtract(imgROI, true);
 
 
         cout << houghVote << "\n";
@@ -146,36 +136,11 @@ int main(int argc, char *argv[]) {
         }
         std::cout << houghVote << "\n";
         Mat result(imgROI.size(), CV_8U, Scalar(255));
+
+        //将ROI深拷贝，得到一个全新的矩阵
         imgROI.copyTo(result);
 
-        // Draw the lines
-        std::vector<Vec2f>::const_iterator it = lines.begin();
-        Mat hough(imgROI.size(), CV_8U, Scalar(0));
-        while (it != lines.end()) {
-
-            float rho = (*it)[0];   // first element is distance rho
-            float theta = (*it)[1]; // second element is angle theta
-
-            if (theta > 0.09 && theta < 1.48 ||
-                theta < 3.14 && theta > 1.66) { // filter to remove vertical and horizontal lines
-
-                // point of intersection of the line with first row
-                Point pt1(rho / cos(theta), 0);
-                // point of intersection of the line with last row
-                Point pt2((rho - result.rows * sin(theta)) / cos(theta), result.rows);
-                // draw a  line : Color = Scalar(R, G, B), thickness
-                line(result, pt1, pt2, Scalar(255, 0, 0), 3);
-                line(hough, pt1, pt2, Scalar(255, 0, 0), 3);
-            }
-
-            //std::cout << "line: (" << rho << "," << theta << ")\n";
-            ++it;
-        }
-
-        // Display the detected line image
-
-
-
+        drawHoughDectedLines(lines, result, true);
 
         // Create LineFinder instance
 //        LineFinder ld;
@@ -191,11 +156,6 @@ int main(int argc, char *argv[]) {
 //        ld.drawDetectedLines(houghP);
 //        std::cout << "First Hough" << "\n";
 
-        if (showHoughP) {
-            namedWindow("Detected Lines with HoughP");
-
-
-        }
 
         // bitwise AND of the two hough images
 //        bitwise_and(houghP, hough, houghP);
@@ -203,10 +163,7 @@ int main(int argc, char *argv[]) {
 //        Mat dst(imgROI.size(), CV_8U, Scalar(0));
 //        threshold(houghP, houghPinv, 150, 255, THRESH_BINARY_INV); // threshold and invert to black lines
 
-        if (showHoughP) {
-            namedWindow("Detected Lines with Bitwise");
 
-        }
 
 //        Canny(houghPinv, contours, 100, 350);
 //        li = ld.findLines(contours);
@@ -251,13 +208,57 @@ void showBasicInformation(VideoCapture &capture) {
 }
 
 
-Mat cannyExtract(Mat &imgROI) {
+Mat cannyExtract(Mat &imgROI, bool showCanny) {
 
     // Canny algorithm
     Mat contours;
     Canny(imgROI, contours, 100, 200);
     Mat contoursInv;
     threshold(contours, contoursInv, 128, 255, THRESH_BINARY_INV);
+
+    // Display Canny image
+    if (showCanny) {
+        imshow("Canny", contours);
+    }
+
+
+
     return contours;
+}
+
+
+void drawHoughDectedLines(vector<Vec2f> &lines, Mat &result, bool showHough) {
+
+    std::vector<Vec2f>::const_iterator it = lines.begin();
+
+    while (it != lines.end()) {
+
+        float rho = (*it)[0];   // first element is distance rho
+        float theta = (*it)[1]; // second element is angle theta
+
+
+        //根据检测出直线的角度信息, 过滤掉不是车道的直线
+        //角度的设置可能tricky
+        if (theta > 0.09 && theta < 1.48 ||
+            theta < 3.14 && theta > 1.88) { // filter to remove vertical and horizontal lines
+
+            // point of intersection of the line with first row
+            Point pt1(rho / cos(theta), 0);
+            // point of intersection of the line with last row
+            Point pt2((rho - result.rows * sin(theta)) / cos(theta), result.rows);
+            // draw a  line : Color = Scalar(R, G, B), thickness
+            line(result, pt1, pt2, Scalar(255, 0, 0), 3);
+        }
+
+        cout << "line: (" << rho << "," << theta << ")\n";
+        ++it;
+    }
+
+    // Display the detected line image
+    if (showHough) {
+        imshow("Hough", result);
+    }
+
+
 }
 
