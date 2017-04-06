647ca294d3f26d348ad77b4af5bfd488da4b26e3
FrancisGee <gaoyikang2014@Gmail.com>
2017-04-06 18:44:32 +0800

    主程序重构

--- main.cpp
+++ main.cpp
@@ -36,11 +36,15 @@ Notes:
 	Added image ROI to reduce false lines from things like trees/powerlines
 \*------------------------------------------------------------------------------------------*/
 
+
+#include <opencv2/core/core.hpp>
 #include "opencv2/highgui/highgui.hpp"
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include "linefinder.h"
+#include "laneDetection.h"
+
 
 #define PI 3.1415926
 
@@ -49,8 +53,6 @@ using namespace std;
 
 void showBasicInformation(VideoCapture &input);
 
-Mat cannyExtract(Mat &imgROI, bool showCanny);
-
 void drawHoughDectedLines(vector<Vec2f> &lines, Mat &result, bool showHough);
 
 vector<Vec2f> houghDected(int initialHoughVote, Mat &image);
@@ -58,6 +60,11 @@ vector<Vec2f> houghDected(int initialHoughVote, Mat &image);
 
 int main(int argc, char *argv[]) {
 //    int houghVote = 200;
+
+
+
+    laneDetection detect; // object of laneDetection class
+
     string arg = argv[1];
 
 
@@ -93,18 +100,35 @@ int main(int argc, char *argv[]) {
         if (input.empty())
             break;
 
-        //固定视频帧的大小，便于检测
+        //固定视频帧的大小，便于检测---width = 800 , height = 600
         Mat image;
-        Size size(1280, 720);
+        Size size(detect._width, detect._height);
         resize(input, image, size);
+        cout << "width = " << image.cols << "  height = " << image.rows << endl;
 
-
-
-
+        //转换为灰度图处理
         Mat gray;
-
-
         cvtColor(image, gray, CV_RGB2GRAY);
+
+        //设置感兴趣区域大小，滤除天空等干扰
+        Rect roi(0, image.cols / 3, image.cols - 1, image.rows - image.cols / 3);// set the ROI for the image
+        Mat grayROI = gray(roi);
+
+
+        //得到垂直方向的边缘??
+//        Mat contours = detect.LMFiltering(grayROI); // Filtering to detect Lane Markings
+//        Mat contours = cannyExtract(grayROI,true);
+//        vector<Vec2f> lines = detect.houghTransform(contours); // Hough Transform
+//        Mat imgFinal = detect.drawLines(image, lines); // draw final Lane Markings on the original image
+//        drawHoughDectedLines(lines, input, true);
+
+
+
+
+//        Mat gray;
+//
+//
+//        cvtColor(image, gray, CV_RGB2GRAY);
 
 //        vector<string> codes; //???
 //        Mat corners;
@@ -113,46 +137,58 @@ int main(int argc, char *argv[]) {
 
         //可能与读取视频的速度有关
 
-        Rect roi(0, image.cols / 3, image.cols - 1, image.rows - image.cols / 3);// set the ROI for the image
+//        Rect roi(0, image.cols / 3, image.cols - 1, image.rows - image.cols / 3);// set the ROI for the image
 
 
 
 
         Mat imgROI = image(roi);
-        Mat grayROI = gray(roi);
-
+//        Mat grayROI = gray(roi);
+//
         imshow("original", grayROI);
 
         //用canny算子对ROI进行特征提取
-        Mat contours = cannyExtract(grayROI, true);
+//        Mat contours = cannyExtract(grayROI, true);
+
+        //垂直边缘检测
+        Mat contours = detect.LMFiltering(grayROI);
+
+        // Display Canny image
+        if (showCanny) {
+            imshow("Canny", contours);
+        }
 
 
         //用常规Hough变换进行直线检测
-        vector<Vec2f> lines = houghDected(200, contours);
+//        vector<Vec2f> lines = houghDected(200, contours);
 
 
         Mat result(imgROI.size(), CV_8U, Scalar(255));
         //将ROI深拷贝，得到一个全新的矩阵
         imgROI.copyTo(result);
 
-        Mat Test(imgROI.size(), CV_8U, Scalar(0));
+//        Mat Test(imgROI.size(), CV_8U, Scalar(0));
 
-        drawHoughDectedLines(lines, result, true);
+//        drawHoughDectedLines(lines, result, true);
 
         //  Create LineFinder instance
         LineFinder ld;
 
         //  Set probabilistic Hough parameters
-        ld.setLineLengthAndGap(90, 10);    //min accepted length and gap
+        ld.setLineLengthAndGap(20, 10);    //min accepted length and gap
         ld.setMinVote(100);    // sit > 3 to get rid of "spiderweb"
 
         //  Detect lines
         std::vector<Vec4i> li = ld.findLines(contours);
+
+
         Mat houghP(imgROI.size(), CV_8U, Scalar(0));
+
+        //根据驾驶员的角度设置偏移量
         ld.setShift(0);
-
+//
         ld.drawDetectedLines(imgROI);
-
+//
         imshow("houghP", imgROI);
 
 
@@ -160,16 +196,17 @@ int main(int argc, char *argv[]) {
 
 
 
-        // bitwise ＯＲ of the two hough images
-//        bitwise_or(houghP, result, image);
+
 
 
         imshow(window_name, image);
 
 
 
-        lines.clear();
 
+
+
+        //交互式操作，按任意键就可以暂停，再按任意键就恢复播放
         int delay = 30;
 
         if (delay >= 0 && waitKey(delay) >= 0)
@@ -187,27 +224,6 @@ void showBasicInformation(VideoCapture &capture) {
 
     std::cout << "Frame Size = " << dWidth << "x" << dHeight << std::endl;
 }
-
-
-Mat cannyExtract(Mat &imgROI, bool showCanny) {
-
-    // Canny algorithm
-    Mat contours;
-    double thresh = 200;
-    Canny(imgROI, contours, 0.5 * thresh, thresh);
-    Mat contoursInv;
-    threshold(contours, contoursInv, 128, 255, THRESH_BINARY_INV);
-
-    // Display Canny image
-    if (showCanny) {
-        imshow("Canny", contours);
-    }
-
-
-
-    return contours;
-}
-
 
 void drawHoughDectedLines(vector<Vec2f> &lines, Mat &result, bool showHough) {
 
