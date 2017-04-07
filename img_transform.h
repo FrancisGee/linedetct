6bfa07f3ad04f5b58c75e8be09c2e18e7c242df5
FrancisGee <gaoyikang2014@Gmail.com>
2017-04-07 11:41:08 +0800

    增加高斯平滑操作

--- img_transform.h
+++ img_transform.h
@@ -12,7 +12,32 @@
 using namespace cv;
 
 
+
 Mat cannyExtract(Mat &imgROI, bool showCanny);
+
+/*
+ * 高斯滤波，让图像更加平滑，消除掉噪声对边缘检测器的干扰。
+ *
+ *
+ *
+ */
+
+class GaussianFilter {
+public:
+    void Init(int _sigma_x, int _sigma_y);
+
+    void Filter(Mat src, Mat &dst);
+
+private:
+    Mat gaussian_kernel_x_;
+    Mat gaussian_kernel_y_;
+    int GAUSSIANSIZE = 5;
+    int sigma_x_;
+    int sigma_y_;
+
+    void onGaussianChange();
+};
+
 
 
 #endif //LINEDETECT_IMG_TRANSFORM_H
