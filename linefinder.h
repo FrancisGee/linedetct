1007ffe192a62e2651d98c9ac1b1692a92530877
FrancisGee <gaoyikang2014@Gmail.com>
2017-04-13 11:37:32 +0800

    将line域去除

--- linefinder.h
+++ linefinder.h
@@ -20,15 +20,17 @@
 #if !defined LINEF
 #define LINEF
 
+
+#include <cv.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <cv.h>
-#include <opencv2/imgproc/imgproc.hpp>
+
 
 #define PI 3.1415926
 
 
 using namespace std;
+using namespace cv;
 
 class LineFinder {
 
@@ -39,7 +41,7 @@ private:
 
     // vector containing the end points
     // of the detected lines
-    std::vector<cv::Vec4i> lines;
+//    std::vector<cv::Vec4i> lines;
 
     // accumulator resolution parameters
     double deltaRho;
@@ -93,14 +95,15 @@ public:
     // Apply probabilistic Hough Transform
     std::vector<cv::Vec4i> findLines(cv::Mat &binary) {
 
-        lines.clear();
+        std::vector<cv::Vec4i> lines;
+//        lines.clear();
         cv::HoughLinesP(binary, lines, deltaRho, deltaTheta, minVote, minLength, maxGap);
 
         return lines;
     }
 
     // Draw the detected lines on an image
-    void drawDetectedLines(cv::Mat &image, cv::Scalar color = cv::Scalar(255)) {
+    void drawDetectedLines(cv::Mat &image, vector<Vec4i> lines) {
         vector<cv::Point> leftEndPoint;
         vector<cv::Point> leftStartPoint;
 
@@ -141,7 +144,7 @@ public:
                 }
 
 
-                cv::line(image, pt1, pt2, color, 3);
+                cv::line(image, pt1, pt2, Scalar(255), 3);
 
 //                  cv::line(image, leftEndPoint[0], leftStartPoint[0], color, 3);
 //                  cv::line(image, rightEndPoint[0], rightStartPoint[0], color, 3);
@@ -180,55 +183,55 @@ public:
     // the ones specified in the input matrix of orientations
     // At least the given percentage of pixels on the line must
     // be within plus or minus delta of the corresponding orientation
-    std::vector<cv::Vec4i> removeLinesOfInconsistentOrientations(
-            const cv::Mat &orientations, double percentage, double delta) {
-
-        std::vector<cv::Vec4i>::iterator it = lines.begin();
-
-        // check all lines
-        while (it != lines.end()) {
-
-            // end points
-            int x1 = (*it)[0];
-            int y1 = (*it)[1];
-            int x2 = (*it)[2];
-            int y2 = (*it)[3];
-
-            // line orientation + 90o to get the parallel line
-
-            double ori1;
-            if (ori1 > PI) ori1 = ori1 - 2 * PI;
-
-            double ori2 = atan2(static_cast<double>(y2 - y1), static_cast<double>(x2 - x1)) + PI / 2;
-            if (ori2 > PI) ori2 = ori2 - 2 * PI;
-
-            // for all points on the line
-            cv::LineIterator lit(orientations, cv::Point(x1, y1), cv::Point(x2, y2));
-            int i, count = 0;
-            for (i = 0, count = 0; i < lit.count; i++, ++lit) {
-
-                float ori = *(reinterpret_cast<float *>(*lit));
-
-                // is line orientation similar to gradient orientation ?
-                if (std::min(fabs(ori - ori1), fabs(ori - ori2)) < delta)
-                    count++;
-
-            }
-
-            double consistency = count / static_cast<double>(i);
-
-            // set to zero lines of inconsistent orientation
-            if (consistency < percentage) {
-
-                (*it)[0] = (*it)[1] = (*it)[2] = (*it)[3] = 0;
-
-            }
-
-            ++it;
-        }
-
-        return lines;
-    }
+//    std::vector<cv::Vec4i> removeLinesOfInconsistentOrientations(
+//            const cv::Mat &orientations, double percentage, double delta) {
+//
+//        std::vector<cv::Vec4i>::iterator it = lines.begin();
+//
+//        // check all lines
+//        while (it != lines.end()) {
+//
+//            // end points
+//            int x1 = (*it)[0];
+//            int y1 = (*it)[1];
+//            int x2 = (*it)[2];
+//            int y2 = (*it)[3];
+//
+//            // line orientation + 90o to get the parallel line
+//
+//            double ori1;
+//            if (ori1 > PI) ori1 = ori1 - 2 * PI;
+//
+//            double ori2 = atan2(static_cast<double>(y2 - y1), static_cast<double>(x2 - x1)) + PI / 2;
+//            if (ori2 > PI) ori2 = ori2 - 2 * PI;
+//
+//            // for all points on the line
+//            cv::LineIterator lit(orientations, cv::Point(x1, y1), cv::Point(x2, y2));
+//            int i, count = 0;
+//            for (i = 0, count = 0; i < lit.count; i++, ++lit) {
+//
+//                float ori = *(reinterpret_cast<float *>(*lit));
+//
+//                // is line orientation similar to gradient orientation ?
+//                if (std::min(fabs(ori - ori1), fabs(ori - ori2)) < delta)
+//                    count++;
+//
+//            }
+//
+//            double consistency = count / static_cast<double>(i);
+//
+//            // set to zero lines of inconsistent orientation
+//            if (consistency < percentage) {
+//
+//                (*it)[0] = (*it)[1] = (*it)[2] = (*it)[3] = 0;
+//
+//            }
+//
+//            ++it;
+//        }
+//
+//        return lines;
+//    }
 };
 
 
